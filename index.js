241d32f088009bb148ac1ea312f6233681fa2125
Anton Narusberg <anton.narusberg@gmail.com>
2015-06-06 13:28:23 +0300

    fabric.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path');
+
+app.use("/public", express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', function(req, res){
   res.sendFile('index.html', { root: __dirname });
